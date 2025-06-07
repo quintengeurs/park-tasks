29f64e3db4abd2f7adb1e6de228f5c06401393aa
quintengeurs <quinten.geurs@gmail.com>
2025-06-07 15:26:26 +0100

    updated files

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://park-tasks.onrender.com', // Replace with your backend Render URL
+      '/api': 'park-tasks:3000', // Replace with your backend Render URL
       '/uploads': 'https://park-staff-backend.onrender.com', // Add for file serving
     },
   },
