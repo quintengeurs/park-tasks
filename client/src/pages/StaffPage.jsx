01c6735f9fab1e7aea7e080a5852387bc3ef0262
quintengeurs <quinten.geurs@gmail.com>
2025-06-10 15:48:08 +0100

    Create StaffPage.jsx

---
+++ client/src/pages/StaffPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StaffPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Staff Profile</h1>
+      <p>Staff placeholder</p>
+    </div>
+  );
+};
+
+export default StaffPage;
