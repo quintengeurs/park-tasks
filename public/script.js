7356fb2b89fffddabb52792f794e7920f9171b71
quintengeurs <quinten.geurs@gmail.com>
2025-07-14 12:03:05 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
   const supabase = createClient(
-      'YOUR_SUPABASE_URL',
-      'YOUR_SUPABASE_ANON_KEY'
+      'https://hpzcmwhdiffcfpteyiui.supabase.co',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwemNtd2hkaWZmY2ZwdGV5aXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzcyNTUsImV4cCI6MjA2NzkxMzI1NX0.x8uwDxLA5lJKDTo9kYqpJN7xLbjDbO_RrVQx2ZOiVdU'
   );
 
   async function checkAuth() {
